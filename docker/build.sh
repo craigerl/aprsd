c1777483405b8c208ae0795e636c1b58390c37df
Hemna <waboring@hemna.com>
2021-02-16 10:05:11 -0500

    Updated Dockerfile for multiplatform builds
    This patch updates the main Dockerfile container build to use
    the python:3.8-slim base image and installs aprsd from pypi.
    This also results in a much smaller image.
    
    Also added support for multiarchitecture builds so the same Dockerfile
    builds for raspberry pi and linux/amd64

--- docker/build.sh
+++ docker/build.sh
@@ -7,5 +7,5 @@ docker buildx build --push --platform linux/arm/v7,linux/arm/v6,linux/arm64,linu
     -t hemna6969/aprsd:$VERSION \
     -t hemna6969/aprsd:latest \
     -t 192.168.1.3:5000/hemna6969/aprsd:latest \
-    -t 192.168.1.3:5000/hemna669/aprsd:$VERSION \
+    -t 192.168.1.3:5000/hemna6969/aprsd:$VERSION \
     -f Dockerfile .
