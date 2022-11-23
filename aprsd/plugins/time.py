528bdb99e73edb94c984c7db7ca00849128e1dc0
Hemna <waboring@hemna.com>
2022-11-23 12:59:02 -0500

    Removed TimeOpenCageDataPlugin
    This patch removes the TimeOpenCageDataPlugin as it's been superceded
    by the aprsd-timeopencage-plugin

--- aprsd/plugins/time.py
+++ aprsd/plugins/time.py
@@ -2,9 +2,6 @@ import logging
 import re
 import time
 
-from opencage.geocoder import OpenCageGeocode
-import pytz
-
 from aprsd import plugin, plugin_utils
 from aprsd.utils import fuzzy, trace
 
@@ -47,68 +44,6 @@ class TimePlugin(plugin.APRSDRegexCommandPluginBase):
         LOG.info("TIME COMMAND")
         # So we can mock this in unit tests
         localzone = self._get_local_tz()
-        return self.build_date_str(localzone)
-
-
-class TimeOpenCageDataPlugin(TimePlugin, plugin.APRSFIKEYMixin):
-    """geocage based timezone fetching."""
-
-    command_regex = "^[tT]"
-    command_name = "time"
-    short_description = "Current time of GPS beacon timezone. Uses OpenCage"
-
-    def setup(self):
-        self.ensure_aprs_fi_key()
-
-    @trace.trace
-    def process(self, packet):
-        fromcall = packet.get("from")
-        message = packet.get("message_text", None)
-        # ack = packet.get("msgNo", "0")
-
-        api_key = self.config["services"]["aprs.fi"]["apiKey"]
-
-        # optional second argument is a callsign to search
-        a = re.search(r"^.*\s+(.*)", message)
-        if a is not None:
-            searchcall = a.group(1)
-            searchcall = searchcall.upper()
-        else:
-            # if no second argument, search for calling station
-            searchcall = fromcall
-
-        try:
-            aprs_data = plugin_utils.get_aprs_fi(api_key, searchcall)
-        except Exception as ex:
-            LOG.error(f"Failed to fetch aprs.fi data {ex}")
-            return "Failed to fetch location"
-
-        # LOG.debug("LocationPlugin: aprs_data = {}".format(aprs_data))
-        if not len(aprs_data["entries"]):
-            LOG.error("Didn't get any entries from aprs.fi")
-            return "Failed to fetch aprs.fi location"
-
-        lat = aprs_data["entries"][0]["lat"]
-        lon = aprs_data["entries"][0]["lng"]
-
-        try:
-            self.config.exists("opencagedata.apiKey")
-        except Exception as ex:
-            LOG.error(f"Failed to find config opencage:apiKey {ex}")
-            return "No opencage apiKey found"
-
-        try:
-            opencage_key = self.config["opencagedata"]["apiKey"]
-            geocoder = OpenCageGeocode(opencage_key)
-            results = geocoder.reverse_geocode(lat, lon)
-        except Exception as ex:
-            LOG.error(f"Couldn't fetch opencagedata api '{ex}'")
-            # Default to UTC instead
-            localzone = pytz.timezone("UTC")
-        else:
-            tzone = results[0]["annotations"]["timezone"]["name"]
-            localzone = pytz.timezone(tzone)
-
         return self.build_date_str(localzone)
 
 
