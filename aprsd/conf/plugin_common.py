99f200840d4fc9262c024385bb906e049254983b
Hemna <waboring@hemna.com>
2023-11-22 20:39:06 -0500

    Rework Location Plugin
    This Patch updates the location plugin to allow configuring which
    geopy library's supported geocoders.  This patch also adds a fake
    geopy geocoder class that uses the us government's API for location.

--- aprsd/conf/plugin_common.py
+++ aprsd/conf/plugin_common.py
@@ -18,6 +18,11 @@ owm_wx_group = cfg.OptGroup(
     title="Options for the OWMWeatherPlugin",
 )
 
+location_group = cfg.OptGroup(
+    name="location_plugin",
+    title="Options for the LocationPlugin",
+)
+
 aprsfi_opts = [
     cfg.StrOpt(
         "apiKey",
@@ -62,6 +67,106 @@ avwx_opts = [
     ),
 ]
 
+location_opts = [
+    cfg.StrOpt(
+        "geopy_geocoder",
+        choices=[
+            "ArcGIS", "AzureMaps", "Baidu", "Bing", "GoogleV3", "HERE",
+            "Nominatim", "OpenCage", "TomTom", "USGov", "What3Words", "Woosmap",
+        ],
+        default="Nominatim",
+        help="The geopy geocoder to use.  Default is Nominatim."
+            "See https://geopy.readthedocs.io/en/stable/#module-geopy.geocoders"
+            "for more information.",
+    ),
+    cfg.StrOpt(
+        "user_agent",
+        default="APRSD",
+        help="The user agent to use for the Nominatim geocoder."
+             "See https://geopy.readthedocs.io/en/stable/#module-geopy.geocoders"
+             "for more information.",
+    ),
+    cfg.StrOpt(
+        "arcgis_username",
+        default=None,
+        help="The username to use for the ArcGIS geocoder."
+             "See https://geopy.readthedocs.io/en/latest/#arcgis"
+             "for more information."
+             "Only used for the ArcGIS geocoder.",
+    ),
+    cfg.StrOpt(
+        "arcgis_password",
+        default=None,
+        help="The password to use for the ArcGIS geocoder."
+             "See https://geopy.readthedocs.io/en/latest/#arcgis"
+             "for more information."
+             "Only used for the ArcGIS geocoder.",
+    ),
+    cfg.StrOpt(
+        "azuremaps_subscription_key",
+        help="The subscription key to use for the AzureMaps geocoder."
+             "See https://geopy.readthedocs.io/en/latest/#azuremaps"
+             "for more information."
+             "Only used for the AzureMaps geocoder.",
+    ),
+    cfg.StrOpt(
+        "baidu_api_key",
+        help="The API key to use for the Baidu geocoder."
+             "See https://geopy.readthedocs.io/en/latest/#baidu"
+             "for more information."
+             "Only used for the Baidu geocoder.",
+    ),
+    cfg.StrOpt(
+        "bing_api_key",
+        help="The API key to use for the Bing geocoder."
+             "See https://geopy.readthedocs.io/en/latest/#bing"
+             "for more information."
+             "Only used for the Bing geocoder.",
+    ),
+    cfg.StrOpt(
+        "google_api_key",
+        help="The API key to use for the Google geocoder."
+             "See https://geopy.readthedocs.io/en/latest/#googlev3"
+             "for more information."
+             "Only used for the Google geocoder.",
+    ),
+    cfg.StrOpt(
+        "here_api_key",
+        help="The API key to use for the HERE geocoder."
+             "See https://geopy.readthedocs.io/en/latest/#here"
+             "for more information."
+             "Only used for the HERE geocoder.",
+    ),
+    cfg.StrOpt(
+        "opencage_api_key",
+        help="The API key to use for the OpenCage geocoder."
+             "See https://geopy.readthedocs.io/en/latest/#opencage"
+             "for more information."
+             "Only used for the OpenCage geocoder.",
+    ),
+    cfg.StrOpt(
+        "tomtom_api_key",
+        help="The API key to use for the TomTom geocoder."
+             "See https://geopy.readthedocs.io/en/latest/#tomtom"
+             "for more information."
+             "Only used for the TomTom geocoder.",
+    ),
+    cfg.StrOpt(
+        "what3words_api_key",
+        help="The API key to use for the What3Words geocoder."
+             "See https://geopy.readthedocs.io/en/latest/#what3words"
+             "for more information."
+             "Only used for the What3Words geocoder.",
+    ),
+    cfg.StrOpt(
+        "woosmap_api_key",
+        help="The API key to use for the Woosmap geocoder."
+             "See https://geopy.readthedocs.io/en/latest/#woosmap"
+             "for more information."
+             "Only used for the Woosmap geocoder.",
+    ),
+]
+
 
 def register_opts(config):
     config.register_group(aprsfi_group)
@@ -72,6 +177,8 @@ def register_opts(config):
     config.register_opts(owm_wx_opts, group=owm_wx_group)
     config.register_group(avwx_group)
     config.register_opts(avwx_opts, group=avwx_group)
+    config.register_group(location_group)
+    config.register_opts(location_opts, group=location_group)
 
 
 def list_opts():
@@ -80,4 +187,5 @@ def list_opts():
         query_group.name: query_plugin_opts,
         owm_wx_group.name: owm_wx_opts,
         avwx_group.name: avwx_opts,
+        location_group.name: location_opts,
     }
