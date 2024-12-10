fcd1629fdebb485fc763e19d73bfdafc10de6251
Hemna <waboring@hemna.com>
2024-12-10 11:48:27 -0500

    removed email reference
    From the list-plugins command

--- aprsd/cmds/list_plugins.py
+++ aprsd/cmds/list_plugins.py
@@ -21,7 +21,7 @@ from aprsd import cli_helper
 from aprsd import plugin as aprsd_plugin
 from aprsd.main import cli
 from aprsd.plugins import (
-    email, fortune, location, notify, ping, time, version, weather,
+    fortune, location, notify, ping, time, version, weather,
 )
 
 
