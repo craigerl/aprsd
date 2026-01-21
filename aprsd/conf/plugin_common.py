cefe3e30e764e6d4293cd7a0485aac39f0412527
Walter Boring <waboring@hemna.com>
2026-01-21 16:27:14 -0500

    Removed AVWX plugin.
    The AVWX Based weather plugin requires a subscription to the AVWX
    API, or to self host a running AVWX API.  So this plugin has been
    moved to it's own external plugin here:
    aprsd-avwx-plugin:
    
    https://github.com/hemna/aprsd-avwx-plugin

--- aprsd/conf/plugin_common.py
+++ aprsd/conf/plugin_common.py
@@ -4,10 +4,6 @@ aprsfi_group = cfg.OptGroup(
     name='aprs_fi',
     title='APRS.FI website settings',
 )
-avwx_group = cfg.OptGroup(
-    name='avwx_plugin',
-    title='Options for the AVWXWeatherPlugin',
-)
 
 aprsfi_opts = [
     cfg.StrOpt(
@@ -16,33 +12,13 @@ aprsfi_opts = [
     ),
 ]
 
-avwx_opts = [
-    cfg.StrOpt(
-        'apiKey',
-        help='avwx-api is an opensource project that has'
-        'a hosted service here: https://avwx.rest/'
-        'You can launch your own avwx-api in a container'
-        'by cloning the githug repo here:'
-        'https://github.com/avwx-rest/AVWX-API',
-    ),
-    cfg.StrOpt(
-        'base_url',
-        default='https://avwx.rest',
-        help='The base url for the avwx API.  If you are hosting your own'
-        'Here is where you change the url to point to yours.',
-    ),
-]
-
 
 def register_opts(config):
     config.register_group(aprsfi_group)
     config.register_opts(aprsfi_opts, group=aprsfi_group)
-    config.register_group(avwx_group)
-    config.register_opts(avwx_opts, group=avwx_group)
 
 
 def list_opts():
     return {
         aprsfi_group.name: aprsfi_opts,
-        avwx_group.name: avwx_opts,
     }
